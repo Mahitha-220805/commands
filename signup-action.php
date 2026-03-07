29006a0ce8a25b6dbe8363902227a63cddb78296
unknown <n220805@rguktn.ac.in>
2026-03-07 18:45:18 +0530

    signup is connected to database

---
+++ signup-action.php
@@ -0,0 +1,24 @@
+<?php
+
+include 'conn.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $username = trim($_POST['username']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    // Clean input
+    $username = strtolower($username);
+}
+$sql = "INSERT INTO users (userame,email,password)
+        VALUES ('$username','$email', '$password')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Details submitted successfully";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
